f93dad48b8560d24071e263bc24dcfaeaeeb7366
Mike Kelly <mkelly32@tcd.ie>
2021-11-25 06:41:10 +0000

    More formatting done on data (to make times more readable)

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import json
 
 def main():
-    f = open('BAW827scraped.json')
-    data = json.load(f)
-    print(data)
+    files = [
+        "BAW827scraped.json",
+        "BAW831scraped.json",
+        "BAW837scraped.json",
+        "BAW839scraped.json",
+        "BAW845scraped.json",
+        "BAW9174scraped.json",
+        "EIN152scraped.json",
+        "EIN154scraped.json",
+        "EIN158scraped.json",
+        "EIN164scraped.json",
+        "EIN168scraped.json",
+        "EIN172scraped.json",
+        "EIN176scraped.json"
+    ]
+
+    flight_data = []
+    for file in files:
+        flight_data.append(json.load(open(file)))
+        
+    for i in range(len(files)):
+        flight = flight_data[i]
+        for j in range(flight.listOfDates):
+            print(j)
 
 if __name__ == "__main__":
     main()
