32fe61aefcfe3306174c7d9c4bd5895cca11e391
Mike Kelly <mkelly32@tcd.ie>
2021-11-25 07:11:57 +0000

    formatted flight data some more, to make it more machine readable

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import json
 
+def calculateDelta(delta: str) -> int:
+    if delta == "(on time)":
+        return 0
+    return 0 
+
 def main():
     files = [
         "BAW827scraped.json",
@@ -18,13 +23,17 @@ def main():
     ]
 
     flight_data = []
+    clean_data = []
     for file in files:
         flight_data.append(json.load(open(file)))
         
     for i in range(len(files)):
         flight = flight_data[i]
-        for j in flight.keys():
-            print(j)
+        for j in range(len(flight["listOfDates"])):
+            clean_data.append({
+                "date": flight["listOfDates"][j],
+                "delta": calculateDelta(flight["listOfDelays"][j][1])
+            })
 
 if __name__ == "__main__":
     main()
